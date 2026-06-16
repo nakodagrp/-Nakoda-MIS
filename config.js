bbae043cb565ed0c86e4c8e1ca9fa68fbbe7ff30
nakodagrp <jitendra@nakodadiagnostics.in>
2026-06-16 16:35:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@
  *  It looks like:  https://script.google.com/macros/s/AKfy....../exec
  * ============================================================ */
 window.NAKODA_CONFIG = {
-  API_URL: "PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE",
+  API_URL: "API_URL: "https://script.google.com/macros/s/AKfycbyqVSShH6jsBFYCFhZ-BMa2G5V4VDLYepufI_enlKWutLsi9Jigzo_EmSKSfbj7pO4/exec",
   APP_NAME: "Nakoda MIS"
 };
